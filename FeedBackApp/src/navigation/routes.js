a7313eb80545920b3e664eaba6923afa78aa5733
Arun Singh <arunk4it@gmail.com>
2023-02-06 20:07:27 +0530

    navigation done merge push main

--- FeedBackApp/src/navigation/routes.js
+++ FeedBackApp/src/navigation/routes.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import {Button, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
-import {Login, Signup, Splash} from '../screens';
+import * as screens from '../screens/index';
 import {Route} from './route';
 
 const Stack = createStackNavigator();
@@ -10,9 +10,9 @@ const Stack = createStackNavigator();
 function MyStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name={Route.Splash} component={Splash} />
-      <Stack.Screen name={Route.Signup} component={Signup} />
-      <Stack.Screen name={Route.Login} component={Login} />
+      <Stack.Screen name={Route.Splash} component={screens.Splash} />
+      <Stack.Screen name={Route.Signup} component={screens.Signup} />
+      <Stack.Screen name={Route.Login} component={screens.Login} />
     </Stack.Navigator>
   );
 }
