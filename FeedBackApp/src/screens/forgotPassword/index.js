6b15edb2a4a138549ddc2371ac430b7006174c06
Arun Singh <arunk4it@gmail.com>
2023-02-07 19:37:52 +0530

    ds store

--- FeedBackApp/src/screens/forgotPassword/index.js
+++ FeedBackApp/src/screens/forgotPassword/index.js
@@ -1,4 +1,11 @@
-import {View, Text, SafeAreaView, Image, TouchableOpacity} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+} from 'react-native';
 import React from 'react';
 import styles from './styles';
 import {
@@ -12,39 +19,43 @@ import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 const ForgotPassword = props => {
   return (
     <CustomHeader>
-      <View style={styles.mainView}>
-        <TouchableOpacity
-          onPress={() => {
-            props.navigation.goBack();
-          }}>
-          <Icon name="arrow-left" size={25} color="#000" />
-        </TouchableOpacity>
-        <View style={styles.imageView}>
-          <Image source={ImagePath.FORGOT_PASSWORD} />
+      <KeyboardAvoidingView>
+        <View style={styles.mainView}>
+          <TouchableOpacity
+            onPress={() => {
+              props.navigation.goBack();
+            }}>
+            <Icon name="arrow-left" size={25} color="#000" />
+          </TouchableOpacity>
+          <View style={styles.imageView}>
+            <Image source={ImagePath.FORGOT_PASSWORD} />
+          </View>
+          <View style={styles.forgotView}>
+            <Text style={styles.textStyle}>Forgot Your Password</Text>
+          </View>
+          <View style={styles.descriptionView}>
+            <Text style={styles.description}>
+              Enter your registered email OR Phone below to receive password
+              reset instruction
+            </Text>
+          </View>
+
+          <CustomTextInput keyboardType={'email-address'} label={'Email'} />
+
+          <View style={styles.descriptionView}>
+            <Text style={styles.remember}>
+              Remember Password ?
+              <TouchableOpacity
+                onPress={() => {
+                  props.navigation.replace(Route.Login);
+                }}>
+                <Text style={styles.loginText}> Login</Text>
+              </TouchableOpacity>
+            </Text>
+          </View>
+          <CustomButtonComponent label={'SEND'} />
         </View>
-        <View style={styles.forgotView}>
-          <Text style={styles.textStyle}>Forgot Your Password</Text>
-        </View>
-        <View style={styles.descriptionView}>
-          <Text style={styles.description}>
-            Enter your registered email OR Phone below to receive password reset
-            instruction
-          </Text>
-        </View>
-        <CustomTextInput keyboardType={'email-address'} label={'Email'} />
-        <View style={styles.descriptionView}>
-          <Text style={styles.remember}>
-            Remember Password ?
-            <TouchableOpacity
-              onPress={() => {
-                props.navigation.replace(Route.Login);
-              }}>
-              <Text style={styles.loginText}> Login</Text>
-            </TouchableOpacity>
-          </Text>
-        </View>
-        <CustomButtonComponent label={'SEND'} />
-      </View>
+      </KeyboardAvoidingView>
     </CustomHeader>
   );
 };
