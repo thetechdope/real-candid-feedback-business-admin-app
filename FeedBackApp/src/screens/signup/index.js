502336adbb0463ea9b771032b6e09ec8863668b9
avadh100rbh <saurabhsinghrathore1997@gmail.com>
2023-02-09 13:12:47 +0530

    otp

--- FeedBackApp/src/screens/signup/index.js
+++ FeedBackApp/src/screens/signup/index.js
@@ -1,12 +1,32 @@
-import {View, Text} from 'react-native';
+import {View, SafeAreaView} from 'react-native';
 import React from 'react';
 import styles from './styles';
 
 const Signup = () => {
   return (
-    <View>
-      <Text>Signup</Text>
-    </View>
+    <SafeAreaView style={{backgroundColor: '#7E50EE'}}>
+      <View
+        style={{
+          height: '100%',
+          width: '100%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          backgroundColor: '#7E50EE',
+        }}>
+        <View
+          style={{
+            justifyContent: 'center',
+            alignItems: 'center',
+            backgroundColor: 'white',
+            height: '100%',
+            marginTop: 70,
+            borderTopStartRadius: 50,
+            borderTopEndRadius: 50,
+            width: '100%',
+          }}>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
